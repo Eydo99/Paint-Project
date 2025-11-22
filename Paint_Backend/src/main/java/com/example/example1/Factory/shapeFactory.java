592caaf293ba5f01ff5d5ee8aa476a3b7bcf3643
Eyad Amr <eyadamr1644@gmail.com>
2025-11-22 20:58:59 +0200

    Revert "handling custom exception"

--- Paint_Backend/src/main/java/com/example/example1/Factory/shapeFactory.java
+++ Paint_Backend/src/main/java/com/example/example1/Factory/shapeFactory.java
@@ -3,18 +3,16 @@ package com.example.example1.Factory;
 import com.example.example1.Model.Circle;
 import com.example.example1.Model.Rectangle;
 import com.example.example1.Model.shape;
-import com.example.example1.exceptions.TypeNotFoundException;
-
 
 public class shapeFactory {
-    public shape createShape(String type) throws TypeNotFoundException {
+    public shape createShape(String type) {
         switch (type.toLowerCase()) {
             case "circle":
                 return new Circle();
             case "rectangle":
                 return new Rectangle();
             default:
-                throw new TypeNotFoundException(type+" is invalid shape");
+                return null;
         }
     }
 }
--- Paint_Backend/src/main/java/com/example/example1/exceptions/InvalidUndoException.java
+++
@@ -1,7 +0,0 @@
-package com.example.example1.exceptions;
-
-public class InvalidUndoException extends Exception {
-    public InvalidUndoException(String message){
-        super(message) ;
-    }
-}
