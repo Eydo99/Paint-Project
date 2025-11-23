d046602a0bb164566b7b81154a54502bf7390e90
nakieb3laa <hany.ayman606@gmail.com>
2025-11-23 18:14:22 +0200

    toolbar added with icons

--- Paint_Frontend/src/app/components/toolbar/toolbar.component.ts
+++ Paint_Frontend/src/app/components/toolbar/toolbar.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './toolbar.component.html',
-  styleUrl: './toolbar.component.css'
+  styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent {
+  activeTool: string = 'select';
+  @Output() toolChange = new EventEmitter<string>();
 
+  setTool(tool: string) {
+    this.activeTool = tool;
+    this.toolChange.emit(tool);
+  }
 }
